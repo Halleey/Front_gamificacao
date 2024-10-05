45575043a2e83440119f3458d65a86679d245267
hallyson <aparecidohallyson@gmail.com>
2024-10-05 16:49:39 -0300

    renderização condicional

--- src/App.js
+++ src/App.js
@@ -1,19 +1,61 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import styles from './App.module.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [isCreator, setCreator] = useState(false);
+  const [isAuthenticate, setIsAuthenticate] = useState(false);
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+    setIsAuthenticate(false);
+    setCreator(false); 
+    navigate('/'); 
+  };
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+        try {
+            const payload = JSON.parse(atob(token.split('.')[1]));
+            const role = payload.role;
+            if (role === 'CREATOR') {
+                setCreator(true);
+            }
+            setIsAuthenticate(true);
+        } catch (error) {
+            console.error('Invalid token format', error);
+            localStorage.removeItem('token');
+        }
+    }
+  }, []);
+
   return (
     <div className={styles.App}>
       <header className={styles["App-header"]}>
         <h1>Sistema del Gatito mañoso</h1>
         <div className={styles["header-buttons"]}>
-          <Link to={'/register'}>
-            <button>Registre-se</button>
-          </Link>
+          {isAuthenticate ? (
+            <>
+              {isCreator && (
+                <Link to={'/create'}>
+                  <button>Adicionar tarefas</button>
+                </Link>
+              )}
+              <button onClick={handleLogout}>Sair</button>
+            </>
+          ) : (
+            <>
+              <Link to={'/register'}>
+                <button>Registre-se</button>
+              </Link>
 
-          <Link to = {'/login'}> 
-          <button>Login </button>
-          </Link>
+              <Link to={'/login'}>
+                <button>Login</button>
+              </Link>
+            </>
+          )}
         </div>
       </header>
 
